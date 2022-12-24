ae1a3a3620ceb4ec2e70833f0239c720ea9b0167
notadamrizk <93144446+notadamrizk@users.noreply.github.com>
2022-12-24 21:26:30 +0400

    test

--- src/Challenges/ACSLChallenge.java
+++ src/Challenges/ACSLChallenge.java
@@ -1,7 +1,11 @@
 package Challenges;
 
+import java.util.Scanner;
+
 public class ACSLChallenge {
     public static void main(String[] args) {
+   Scanner scanner = new Scanner(System.in);
 
     }
+
 }
